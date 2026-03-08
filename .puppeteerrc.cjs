3ca28c5f676cd6a6f0cc3dd0ebd781fdc9954daa
Danny <dannypirouz@gmail.com>
2026-03-08 15:24:29 -0700

    fix exec path

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,8 +1,5 @@
 const { join } = require('path');
 
-/**
- * @type {import("puppeteer").Configuration}
- */
 module.exports = {
-  cacheDirectory: join('/opt/render/.cache/puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
