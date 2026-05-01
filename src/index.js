93d83fb4c1d196bd44d8b31fa2bde1e0ded1956a
Danny <dannypirouz@gmail.com>
2026-04-30 21:46:22 -0700

    add ducks

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ function calculateUserPoints(userPrediction) {
         };
     }
 
-    const round1Winners = ['Canes', 'Avs', 'Flyers', 'Wild'];
+    const round1Winners = ['Canes', 'Avs', 'Flyers', 'Wild', 'Ducks'];
     const round2Winners = [];
     const round3Winners = [];
     const finalWinner = "";
